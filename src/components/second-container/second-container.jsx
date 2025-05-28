1ba5d7791576a59284ffcd1cdc05142421f8eb13
Brittany-Potato <Brittanycc@missionreadyhq.com>
2025-05-28 22:27:52 +1200

    Added a photo and intro paragraph

--- src/components/second-container/second-container.jsx
+++ src/components/second-container/second-container.jsx
@@ -13,7 +13,11 @@ export default function secondContainer() {
 
             </div>
             <div className={styles.introCard}>
-                
+                                <h3 className={styles.introTitle}>Who am I?</h3>
+                <p className={styles.introParagraph}> 27 years old Full stack developer & level 5 student with Mission Ready. Working with HTML, CSS and JavaScript.
+                    and a knowledge of react and node.js. I have undertaken a variety of projects that can be found on my github, from missions
+                    given by Mission ready, to a discord bot and a growing "dictionary" of JavaScript.</p>
+                <img src="/images/tatoandeevee.jpg" alt="" className={styles.displayPicture}/>
             </div>
         </div>
     </div>
