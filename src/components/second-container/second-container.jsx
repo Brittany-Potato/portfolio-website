d7ddd4c64c1120ad2c32d0f657bec2bd80bd4e5a
Brittany-Potato <Brittanycc@missionreadyhq.com>
2025-05-29 17:31:52 +1200

    Started adding card 1 and button

--- src/components/second-container/second-container.jsx
+++ src/components/second-container/second-container.jsx
@@ -1,25 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./second-container.module.css";
-import {Menu, X} from 'lucide-react';
+import { Menu, X } from "lucide-react";
 
 export default function secondContainer() {
   return (
     <div className={styles.mainContainer}>
-        <div className={styles.cardsDiv}>
-            <div className={styles.cardOne}>
-
-            </div>
-            <div className={styles.cardTwo}>
-
-            </div>
-            <div className={styles.introCard}>
-                                <h3 className={styles.introTitle}>Who am I?</h3>
-                <p className={styles.introParagraph}> 27 years old Full stack developer & level 5 student with Mission Ready. Working with HTML, CSS and JavaScript.
-                    and a knowledge of react and node.js. I have undertaken a variety of projects that can be found on my github, from missions
-                    given by Mission ready, to a discord bot and a growing "dictionary" of JavaScript.</p>
-                <img src="/images/tatoandeevee.jpg" alt="" className={styles.displayPicture}/>
-            </div>
+      <div className={styles.cardsDiv}>
+        <div className={styles.cardOne}>
+          <div className={styles.cardOneInsdeDiv}>
+            <button className={styles.cardOneButton}>
+              <a
+                href="https://github.com/Brittany-Potato"
+                className={styles.cardOneButtonWriting}
+              >
+                Github Repository
+              </a>
+            </button>
+          </div>
         </div>
+        <div className={styles.cardTwo}></div>
+        <div className={styles.introCard}>
+          <h3 className={styles.introTitle}>Who am I?</h3>
+          <p className={styles.introParagraph}>
+            {" "}
+            27 years old Full stack developer & level 5 student with Mission
+            Ready. Working with HTML, CSS and JavaScript. and a knowledge of
+            react and node.js. I have undertaken a variety of projects that can
+            be found on my github, from missions given by Mission ready, to a
+            discord bot and a growing "dictionary" of JavaScript.
+          </p>
+          <img
+            src="/images/tatoandeevee.jpg"
+            alt=""
+            className={styles.displayPicture}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
